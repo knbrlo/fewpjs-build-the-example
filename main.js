05e601319609f56d80d458d2687453016744981b
Ken Barlow <kenbrlow@gmail.com>
2021-03-07 10:00:17 -0700

    commit tree 66353c93598b384b7863696c07feca836e5a9c99

--- main.js
+++ main.js
@@ -4,8 +4,66 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+window.addEventListener("load", (event) => {
+  addEventListenersToHearts()
+  dontAllowListItemsSelect();
+});
 
+function addEventListenersToHearts() {
+  let listOfHearts = document.querySelectorAll('.like-glyph');
+  for (let heart of listOfHearts) {
+    heart.addEventListener("click", function(event) {
+      handleHeartClicked(event.target);
+    })
+  }
+}
 
+function dontAllowListItemsSelect() {
+  let listOfListItems = document.querySelectorAll('.like');
+  for (let item of listOfListItems) {
+    item.classList.add("unselectable");
+  }
+}
+
+function handleHeartClicked(targetHeart) {
+  console.log('heart clicked');
+  mimicServerCall()
+  .then(function(response) {
+    toggleHeart(targetHeart);
+  })
+  .catch(function(error) {
+    if (error != null) {
+      displayErrorMessage(error);
+    }
+  });
+}
+
+function toggleHeart(targetHeart) {
+  if (targetHeart.classList.contains("activated-heart")) {
+    targetHeart.classList.remove("activated-heart");
+  } else {
+    targetHeart.classList.add("activated-heart");
+  }
+}
+
+function displayErrorMessage(errorMessage) {
+  let errorMessageElement = document.getElementById("modal-message");
+  errorMessageElement.innerText = errorMessage;
+  let errorElement = document.getElementById("modal");
+  errorElement.classList.remove("hidden");
+  hideMessageAfterShown();
+}
+
+function hideMessageAfterShown() {
+  setTimeout(function() {
+    hideErrorMessage();
+  }, 5000);
+}
+
+function hideErrorMessage() {
+  let errorElement = document.getElementById("modal");
+  errorElement.classList.add("hidden");
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
